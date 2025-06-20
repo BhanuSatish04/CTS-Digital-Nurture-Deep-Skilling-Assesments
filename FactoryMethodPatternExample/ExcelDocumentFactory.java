dd40b0a32ef3fcd10f3dcd51da349993e3409659
Bhanu Satish <143258795+BhanuSatish04@users.noreply.github.com>
2025-06-20 22:08:50 +0530

    Delete FactoryMethodPatternExample directory

--- FactoryMethodPatternExample/ExcelDocumentFactory.java
+++
@@ -1,7 +0,0 @@
-public class ExcelDocumentFactory extends DocumentFactory {
-    @Override
-    public Document createDocument() {
-        System.out.println("ExcelDocumentFactory: Creating Excel Document instance");
-        return new ExcelDocument();
-    }
-}
--- FactoryMethodPatternExample/WordDocumentFactory.java
+++
@@ -1,8 +0,0 @@
-public class WordDocumentFactory extends DocumentFactory {
-    @Override
-    public Document createDocument() {
-        System.out.println("WordDocumentFactory: Creating Word Document instance");
-        return new WordDocument();
-    }
-}
-
